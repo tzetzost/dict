3c0632df864a83a4452c8272933bddad86f227b7
Tzvetan Stefanov <tstefanov01@pros.com>
2024-03-22 12:31:01 +0200

    Initial setup

---
+++ src/test/java/org/tstefanov/dict/DictApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tstefanov.dict;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DictApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
